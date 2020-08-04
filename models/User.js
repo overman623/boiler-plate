1de3a0e5fb918a0f9fe1fabcce6e3ba6ca24867b
KimBeomjin <overman623@naver.com>
2020-08-04 22:18:54 +0900

    fix(User) : password 암호화 로직 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //sort글자수
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,25 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ){
+
+    var user = this;
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+
+})
+//유저모델에 저장하기전 함수 동작
 
 const User = mongoose.model('User', userSchema)
 
