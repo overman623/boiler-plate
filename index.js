bd5432b529d0f8b165b2f11b04c8dbc930c8c8f7
KimBeomjin <overman623@naver.com>
2020-08-05 15:17:05 +0900

    feat : user authentication 구현

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User');
 
 const config = require('./config/key');
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //회원가입할때 필요 정보 client에서 가져옴
     //그것들을 DB에 넣음
     const user = new User(req.body)
@@ -36,7 +37,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email}, (err, user) => {
@@ -64,6 +65,21 @@ app.post('/login', (req, res) => {
   
   })
 
+})
+
+//role 0은 일반유저 0이 아니면 admin
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기까지 미들웨어를 통과해 왔다는 말은 Authentication이 true라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 app.listen(port, () => {
