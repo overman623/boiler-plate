0d51d87aa5efee276aac1023e585e6c35c6fc65f
KimBeomjin <overman623@naver.com>
2020-08-09 22:08:41 +0900

    feat : redux 추가, chrome redux extension추가

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+// import user from 'user_reducer';
+// 여러개의 리듀서에서 하나로 합치게 된다.
+
+const rootReducer = combineReducers({
+    //user
+    //comment
+    // 등의 기능마다 추가해야한다.
+})
+
+
+export default rootReducer;
