3811cead8378fbfa085bc11f08fd427f711b6094
KimBeomjin <overman623@naver.com>
2020-08-09 22:56:46 +0900

    feat : 로그인 페이지 기본 추가
    react hook 이용/ 메소드에서 useState사용 / 로그인 기본 화면 작성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,14 +6,17 @@ function LandingPage() {
     useEffect(() => {
         axios.get('/api/test').then(
             response => {
-                console.log(response.data)
+                console.log(response)
             }
         )
     }, [])
 
     return (
-        <div>
-            LandingPage
+        <div style={
+            {display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: "100vh"
+            }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }
